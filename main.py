9ce1fc97d0c5a706ebff6473a43883afdd629cc2
danechka2228 <d97944535@gmail.com>
2025-02-13 19:36:54 +0300

    commit1

---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+
+def move_by_keys(event):
+    if event.keysym == 'Up':
+        canvas.move(oval, 0, -20)
+    elif event.keysym == 'Down':
+        canvas.move(oval, 0, 20)
+    elif event.keysym == 'Left':
+        canvas.move(oval, -20, 0)
+    elif event.keysym == 'Right':
+        canvas.move(oval, 20, 0)
+
+
+win = tk.Tk()
+label = tk.Label(win, text='INGINIRIUM')
+label.pack()
+canvas = tk.Canvas(win, bg='#fff', width=700, height=700)
+oval = canvas.create_oval((300, 300), (400, 400), fill='#8171a3')
+canvas.pack()
+win.bind("<KeyPress>", move_by_keys)
+win.mainloop()
